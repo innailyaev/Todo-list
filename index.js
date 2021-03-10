5b602ae73c1881ee1d6fd13b125ba03c41781422
Inna Ilyaev <inna4ilyaev@gmail.com>
2021-03-10 22:39:14 +0200

    10/03/21#2

--- index.js
+++ index.js
@@ -74,31 +74,30 @@ function addTodo() {
 form.addEventListener("submit",  (e)=> {
     e.preventDefault();
     addTodo(); 
-    // const text = document.createElement("p");
-    // const buttonRemove = document.createElement("button");
-    // const buttonEdit = document.createElement("button");
-    // const checkbox = document.createElement("input");
-    // const item = document.createElement("li");
-    let item=`<li id=${task.getLastTask().id}>
-                <input type='checkbox' class='checkbox'> 
-                <button class='edit'>X</button>
-                <p class=${task.getLastTask().id}>${task.getLastTask().text}<p>
-                <button class='buttonRemove'>Edit</button>
-                </li>`
-    // item.setAttribute('id', task.getLastTask().id);
-    // text.setAttribute('class', task.getLastTask().id);
-    let buttonRemove=document.querySelector('.buttonRemove');
-    let checkbox=document.querySelector('.checkbox');
-    // text.innerText = task.getLastTask().text;
-    // buttonRemove.innerText = "X";
-    // buttonEdit.innerHTML='Edit'
-    // checkbox.type = "checkbox";  
+    const text = document.createElement("p");
+    const buttonRemove = document.createElement("button");
+    const buttonEdit = document.createElement("button");
+    const checkbox = document.createElement("input");
+    const item = document.createElement("li");
+    // let item=`<li id=${task.getLastTask().id}>
+    //             <input type='checkbox' class='checkbox'> 
+    //             <button class='edit'>X</button>
+    //             <p class=${task.getLastTask().id}>${task.getLastTask().text}<p>
+    //             <button class='buttonRemove'>Edit</button>
+    //             </li>`
+    item.setAttribute('id', task.getLastTask().id);
+    text.setAttribute('class', task.getLastTask().id);
+    buttonRemove.setAttribute('class','buttonRemove');
+    text.innerText = task.getLastTask().text;
+    buttonRemove.innerText = "X";
+    buttonEdit.innerHTML='Edit'
+    checkbox.type = "checkbox";  
     input.value = null;
-    // item.appendChild(checkbox);
-    // item.appendChild(buttonEdit);
-    // item.appendChild(text);
-    // item.appendChild(buttonRemove);
-    todo+=item;
+    item.appendChild(checkbox);
+    item.appendChild(buttonEdit);
+    item.appendChild(text);
+    item.appendChild(buttonRemove);
+    todo.appendChild(item);
     
     
 
